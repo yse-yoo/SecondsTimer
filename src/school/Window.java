23e3ee895ca2dfd1d9a4c02c83d905d6ec677c64
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-06 14:35:37 +0900

    change images

--- src/school/Window.java
+++ src/school/Window.java
@@ -12,15 +12,15 @@ public class Window extends JFrame{
 	//画像の表示場所
 	public JLabel label;
 	//画像の枚数
-//	public int imageCount = 6;
-	public int imageCount = 5;
+	public int imageCount = 6;
+//	public int imageCount = 5;
 	//アイコン画像の配列（Array）
 	public ImageIcon[] images = new ImageIcon[imageCount];
 	//画像の番号
 	public int index = 0;
 	//画像の先頭文字（プリフィックス）
-//	final String imagePrefix = "school-";
-	final String imagePrefix = "Dance";
+	final String imagePrefix = "school-";
+//	final String imagePrefix = "Dance";
 	//画像の拡張子（Extention）
 	final String imageExt = ".jpg";
 
