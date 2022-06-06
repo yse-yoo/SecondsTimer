840d5c128fdca3e6d613baae93332abd0d8d0283
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-06 10:33:58 +0900

    add SecondsTimer class

--- src/sample/SecondsTimer.java
+++ src/sample/SecondsTimer.java
@@ -2,7 +2,12 @@ package sample;
 
 public class SecondsTimer {
 
+	//秒
+	public static int seconds = 3;
+
 	public static void main(String[] args) {
+
+		System.out.println(seconds + "秒後に実行します");
 
 	}
 
