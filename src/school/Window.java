bd6550c83d517273d7aa4716d16e57abaef9ca0f
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-06 13:08:05 +0900

    add Window Field

--- src/school/Window.java
+++ src/school/Window.java
@@ -1,5 +1,19 @@
 package school;
 
-public class Window {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class Window extends JFrame{
+
+	//画像の表示場所
+	public JLabel label;
+	//画像の枚数
+	public int imageCount = 6;
+	//アイコン画像の配列（Array）
+	public ImageIcon[] images = new ImageIcon[imageCount];
+	//画像の番号
+	public int index = 0;
+	final String imagePrefix = "school-";
+	final String imageExt = ".jpg";
 }
